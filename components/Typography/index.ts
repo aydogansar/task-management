b1ccfe31c35ab42603edbf040f7542619bb8658d
Aydoğan Sarı <aydogansari@hotmail.com>
2022-10-15 21:13:03 +0300

    Export Typography components

---
+++ components/Typography/index.ts
@@ -0,0 +1,3 @@
+export { default as Text } from './Text';
+export { default as Paragraph } from './Paragraph';
+export { default as Title } from './Title';
---
+++ components/index.ts
@@ -0,0 +1,1 @@
+export { Text, Paragraph, Title } from './Typography';
