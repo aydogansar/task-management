a9dc96c1f2c4cff2c3a58e719b7a096fd8868958
Aydoğan Sarı <aydogansari@hotmail.com>
2022-10-15 22:12:19 +0300

    Fix import order

--- store/theme.ts
+++ store/theme.ts
@@ -1,7 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 import type { RootState } from './';
-import type { PayloadAction } from '@reduxjs/toolkit';
 import type { ThemeKeys } from 'types';
 
 interface States {
