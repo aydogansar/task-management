c4d8cc1396f6cbfa222b0ac92fce0a8e4af17e73
Aydoğan Sarı <aydogansari@hotmail.com>
2023-02-11 01:41:24 +0300

    Update config files

--- jest.config.js
+++ jest.config.js
@@ -6,8 +6,10 @@ const createJestConfig = nextJest({
 
 /** @type {import('jest').Config} */
 const customJestConfig = {
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
+  modulePathIgnorePatterns: ['cypress'],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
