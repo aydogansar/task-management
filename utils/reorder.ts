8d0ced9e8d4847ce1ca182a9891317224f0aa8b5
Aydoğan Sarı <aydogansari@hotmail.com>
2023-02-11 16:49:01 +0300

    Add reorder util

---
+++ utils/reorder.ts
@@ -0,0 +1,8 @@
+function reorder<T>(list: Array<T>, startIndex: number, endIndex: number) {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+}
+export default reorder;
