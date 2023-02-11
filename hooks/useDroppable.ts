e4ef69659f67625af456a01c4d3120d0c2940115
Aydoğan Sarı <aydogansari@hotmail.com>
2023-02-11 17:24:51 +0300

    Add comments for useDroppable

--- hooks/useDroppable.ts
+++ hooks/useDroppable.ts
@@ -3,6 +3,11 @@ import { useState, useEffect } from 'react';
 function useDroppable() {
   const [enabled, setEnabled] = useState(false);
 
+  /**
+   * React-beautiful-dnd has issue with react 18 StrictMode
+   * see: https://github.com/atlassian/react-beautiful-dnd/issues/2350
+   * this hook solve issue instead of reactStrictMode set false
+   */
   useEffect(() => {
     const animation = requestAnimationFrame(() => setEnabled(true));
 
